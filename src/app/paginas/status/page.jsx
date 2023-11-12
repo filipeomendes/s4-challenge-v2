a5e720f4ec32bcd4feafee8afa350283d9c961de
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-11-12 20:33:25 -0300

    Adicionando pagina de status

---
+++ src/app/paginas/status/page.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+export const metadata = {
+    title: 'Status',
+}
+
+export default function Status() {
+    return (
+      <main>
+        <div>
+            <h1>Status</h1>
+        </div>
+        <div>
+            <Image src="" alt=""></Image>
+        </div>
+      </main>
+    )
+  }
