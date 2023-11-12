03ee570ce7ae805591901999fc880b8552807ec6
fernandopaparelli <rm551022@fiap.com.br>
2023-11-12 03:39:59 -0300

    preparando import de cabecalho e rodape

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Cabecalho from '@/components/Cabecalho/Cabecalho'
+import Rodape from '@/components/Rodape/Rodape'
 
 const inter = Inter({ subsets: ['latin'] })
 
