696d988d043339a1583afe51a83458fdd18a4a07
MironMarins <mironcbio@gmail.com>
2023-11-12 03:59:17 -0300

    Adicionando imports no cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,3 +1,8 @@
+"use client";
+import Link from "next/link";
+import Image from 'next/image'
+import { Children } from "react";
+
 export default function Cabecalho(){
     return(
         <header>
