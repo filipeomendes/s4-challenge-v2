617f036f6ea89eb1c1e95dfdba59a27c4cfef638
filipeomendes <filipedeoliveiramendes@gmail.com>
2023-11-12 20:35:09 -0300

    Adicionando Classes

--- src/app/paginas/status/page.jsx
+++ src/app/paginas/status/page.jsx
@@ -7,10 +7,10 @@ export const metadata = {
 export default function Status() {
     return (
       <main>
-        <div>
+        <div className="titulo">
             <h1>Status</h1>
         </div>
-        <div>
+        <div className="status-foto">
             <Image src="/status-foto.svg" alt="ilustração do status da solicitação de guincho (guincho a caminho)"></Image>
         </div>
       </main>
