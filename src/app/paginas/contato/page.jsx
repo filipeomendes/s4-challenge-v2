26c00e8b96803b2f1411ab2da3df4c898da02505
vinips1993 <RM550907@fiap.com.br>
2023-11-12 04:16:24 -0300

    Iniciando estrutura da pagina de contatos

---
+++ src/app/paginas/contato/page.jsx
@@ -0,0 +1,21 @@
+export default function Contato() {
+    return (
+      <main>
+        <div>
+          <h1>Contato</h1>
+        </div>
+        <div>
+            <h2></h2>
+            <p></p>
+            <h2></h2>
+            <p></p>
+            <h2></h2>
+            <p></p>
+            <h2></h2>
+            <p></p>
+            <h2></h2>
+            <p></p>
+        </div>
+      </main>
+    )
+  }
