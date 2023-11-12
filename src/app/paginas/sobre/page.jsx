6f7e6af12c3bbd9805a1c9cadd833f78d819f9dd
vinips1993 <RM550907@fiap.com.br>
2023-11-12 04:19:28 -0300

    Iniciando estrutura pagina sobre

---
+++ src/app/paginas/sobre/page.jsx
@@ -0,0 +1,13 @@
+export default function Sobre() {
+    return (
+      <main>
+        <div>
+          <h1>Sobre o Projeto</h1>
+        </div>
+        <div>
+          <h3>Nós da complex viemos para ajudar a Porto Seguro à resolver o desafio que nos foi entregue, e qual seria esse desafio?</h3>
+          <p>Quando nosso carro do dia a dia apresenta algum problema no qual não é possível utilizá-lo, seja por conta de uma peça quebrada, acidente de carro, etc, é comum que seja necessário um guincho para rebocar o veículo até um pátio ou uma oficina, o guincho chega e leva o veículo sem muitos problemas, certo? Mas a situação não é a mesma quando falamos sobre veículos pesados, é frequente que quando um veículo pesado que precise de um guincho para rebocá-lo e esse guincho não aguentar o peso desse veículo. Ou seja, a assertividade de um guincho adequado para veículos pesados não é eficaz, a Porto nos procurou e nós troxemos a solução!</p>
+        </div>
+      </main>
+    )
+  }
