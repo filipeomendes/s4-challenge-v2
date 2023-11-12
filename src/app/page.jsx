127deb87dc5353a6fd0824bb8d2e8ce180482ddd
fernandopaparelli <rm551022@fiap.com.br>
2023-11-12 03:41:48 -0300

    Estruturando a pagina pricipal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
+      <main>
+        <div>
+          <div>
+            <Link href="/maps/solicitacao">
+              <button type="submit" className="botao-seguinte">Seguinte</button>
+            </Link>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
