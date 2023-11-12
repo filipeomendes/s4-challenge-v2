d244b9cfe63cbf959c38c21480e1bf46e039640f
MironMarins <mironcbio@gmail.com>
2023-11-12 04:03:43 -0300

    Adicionando classes nos itens do rodape

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -3,16 +3,16 @@ import Image from "next/image"
 export default function Rodape(){
     return(
         <footer>
-            <section>
-                <div>
-                    <nav>
+            <section className="conteudo-rodape">
+                <div className="redes-sociais-logos">
+                    <nav className="redes-sociais-nav">
                         <a href="https://github.com/filipeomendes/sprint3-challenge-1tdspv" target='blank'><Image src="/github.png" alt="" width={30} height={30}></Image></a>
                         <a href="https://www.instagram.com/complex.wrld/" target='blank'><Image src="/instagram.png" alt="" width={30} height={30}></Image></a>
                         <a href="https://www.facebook.com" target='blank'><Image src="/facebook.png" alt="" width={30} height={30}></Image></a>
                     </nav>
                 </div>
-                <div>
-                    <p>Todos os direitos reservados ©2023 - Complex</p>
+                <div className='copyright'>
+                    <p className='texto-footer'>Todos os direitos reservados ©2023 - Complex</p>
                 </div>
             </section>
         </footer>
