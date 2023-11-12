8c88584e73c8ad1718a9cefc262836250e7a0f04
vinips1993 <RM550907@fiap.com.br>
2023-11-12 04:16:56 -0300

    Adicionando integrantes para contato

--- src/app/paginas/contato/page.jsx
+++ src/app/paginas/contato/page.jsx
@@ -5,16 +5,16 @@ export default function Contato() {
           <h1>Contato</h1>
         </div>
         <div>
-            <h2></h2>
-            <p></p>
-            <h2></h2>
-            <p></p>
-            <h2></h2>
-            <p></p>
-            <h2></h2>
-            <p></p>
-            <h2></h2>
-            <p></p>
+            <h2>Fernando Paparelli Aracena - rm551022</h2>
+            <p>rm551022@fiap.com</p>
+            <h2>Filipe de Oliveira Mendes - rm98959</h2>
+            <p>rm98959@fiap.com.br</p>
+            <h2>Miron Gonçalves Marins - rm551801</h2>
+            <p>rm551801@fiap.com.br</p>
+            <h2>Paulo Henrique de Andrade Junior - rm99714 </h2>
+            <p>rm99714@fiap.com.br</p>
+            <h2>Vinicius Pedro de Souza - rm550907</h2>
+            <p>rm550907@fiap.com.br</p>
         </div>
       </main>
     )
