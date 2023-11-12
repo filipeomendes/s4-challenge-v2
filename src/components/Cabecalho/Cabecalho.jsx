ab341d77e2d075070ca6ae723d57a3699f0107ed
MironMarins <mironcbio@gmail.com>
2023-11-12 03:58:26 -0300

    Adicionando textos Linkaveis no cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -1,16 +1,16 @@
 export default function Cabecalho(){
     return(
         <header>
-            <div className="logo-porto">
+            <div>
                 <Link href="/"><Image src="/logotipo1.svg" alt="" width={30} height={30}></Image></Link>
             </div>
-            <div className="menu">
+            <div>
                 <nav>
-                    <Link href="/maps"></Link>
+                    <Link href="/maps">Solicitação</Link>
                     <span> | </span>
-                    <Link href="/sobre"></Link>
+                    <Link href="/paginas/sobre">Sobre</Link>
                     <span> | </span>
-                    <Link href="/contato"></Link>
+                    <Link href="/paginas/contato">Contato</Link>
                 </nav>
                 <Link href="/" title="Sair da Conta"><Image src="/usuario.svg" alt="" width={50} height={50}></Image></Link>
             </div>
