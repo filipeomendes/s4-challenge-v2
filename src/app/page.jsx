39deb5026a0657d410272233ce08a40ee9c58be4
pauloh-andrade <pauloh8755@gmail.com>
2023-11-13 00:57:12 -0300

    iniciando estilização main mapa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Maps() {
     <>
       <main className="main-mapa">
         <div className="img-mapa">
-          <Image src="/mapa.svg" alt="imagem ilustrativa mapa" width={320} height={320}></Image>
+          <Image src="/mapa.svg" alt="imagem ilustrativa mapa" width={320} height={320}/>
           <div className="botao">
             <Link href="/paginas/solicitacao">
               <button type="submit" className="botao-seguinte">Seguinte</button>
