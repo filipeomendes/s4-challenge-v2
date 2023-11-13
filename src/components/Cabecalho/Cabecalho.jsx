c5d60a0f44102a9695c3abeace14d8938107fb5e
pauloh-andrade <pauloh8755@gmail.com>
2023-11-13 00:31:55 -0300

    Ajustando cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -7,17 +7,17 @@ export default function Cabecalho(){
     return(
         <header>
             <div className="logo-porto">
-                <Link href="/"><Image src="/logotipo1.svg" alt="Logotipo Porto Seguro-SOS" width={30} height={30}></Image></Link>
+                <Link href="/"><Image src="/logotipo1.svg" alt="Logotipo Porto Seguro-SOS" width={100} height={100}></Image></Link>
             </div>
             <div className="menu">
-                <nav>
+                <nav className="menu-2">
                     <Link href="/paginas/solicitacao">Solicitação</Link>
                     <span> | </span>
                     <Link href="/paginas/sobre">Sobre</Link>
                     <span> | </span>
                     <Link href="/paginas/contato">Contato</Link>
                 </nav>
-                <Link href="/login" title="Sair da Conta"><Image src="/usuario.svg" alt="" width={50} height={50}></Image></Link>
+                <Link href="/login" title="Sair da Conta"><Image src="/usuario.svg" alt="" width={30} height={30}></Image></Link>
             </div>
         </header>
     )
